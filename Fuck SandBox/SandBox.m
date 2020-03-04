d876b94786940bee05ae2a7a504977b82d55eecb
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-03-04 23:58:30 +0330

    add classes

--- Fuck SandBox/SandBox.m
+++ Fuck SandBox/SandBox.m
@@ -7,12 +7,30 @@
 //
 
 #import "SandBox.h"
+#include <objc/runtime.h>
 
 @implementation SandBox
 
 - (void) retrieveData {
     NSLog(@"Now you can see me");
+    int i = 0;
+    Class LSApplicationWorkspace = objc_getClass("LSApplicationWorkspace");
+    Class LSApplicationProxy = objc_getClass("LSApplicationProxy");
+    
+    NSObject *defaultWorkspace = [LSApplicationWorkspace performSelector:@selector(defaultWorkspace)];
+    NSArray *allInstalledApplications = [defaultWorkspace performSelector:@selector(allInstalledApplications)];
+//    NSLog(@"%@", allInstalledApplications);
+    
+    for (i = 0; i < allInstalledApplications.count; i++) {
+            NSObject *app = allInstalledApplications[i];
+            if ([app isKindOfClass:LSApplicationProxy]) {
+                NSString* name = [app performSelector:NSSelectorFromString(@"localizedName")];
+                NSString* bundle = [app performSelector:NSSelectorFromString(@"applicationIdentifier")];
+                NSString * type = [app performSelector:NSSelectorFromString(@"applicationType")];
+                NSString * shortVersionString = [app performSelector:NSSelectorFromString(@"shortVersionString")];
+                NSLog(name);
+        }
+    }
 }
-
 
 @end
--- Fuck SandBox/ViewController.swift
+++ Fuck SandBox/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let instanceOfCustomObject = SandBox()
-        instanceOfCustomObject.retrieveData()
+        let sandBox = SandBox()
+        sandBox.retrieveData()
         
     }
 
